51228bcec6cfa4005b50780eff25b97bb87fc1cd
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-11-16 13:45:28 +0000

    Define mbrainz metrics semantics via Telemetry

--- apps/mbrainz/lib/mbrainz/metrics.ex
+++ apps/mbrainz/lib/mbrainz/metrics.ex
@@ -1,6 +1,17 @@
 defmodule Mbrainz.Metrics do
   def setup! do
     :ok = connect_statix!()
+    attach_telemetry_events!()
+  end
+
+  def send_metric([:mbrainz, :api, :ok], duration, meta, _config) do
+    action = Map.get(meta, :action, :unknown)
+    Mbrainz.Metrics.Statix.timing("mbrainz_api.success", duration, tags: ["action:#{action}"])
+  end
+
+  def send_metric([:mbrainz, :api, :error], duration, meta, _config) do
+    action = Map.get(meta, :action, :unknown)
+    Mbrainz.Metrics.Statix.timing("mbrainz_api.error", duration, tags: ["action:#{action}"])
   end
 
   defp connect_statix!() do
@@ -10,5 +21,20 @@ defmodule Mbrainz.Metrics do
     )
 
     :ok = Mbrainz.Metrics.Statix.connect()
+  end
+
+  defp attach_telemetry_events! do
+    events = [
+      [:mbrainz, :api, :ok],
+      [:mbrainz, :api, :error]
+    ]
+
+    Telemetry.attach_many(
+      "mbrainz-statix",
+      events,
+      __MODULE__,
+      :send_metric,
+      nil
+    )
   end
 end
